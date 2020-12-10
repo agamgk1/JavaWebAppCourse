efddb60f0470cc93a853a767c2ddfe854a7c86e3
Adam Gierek <gierek.adam93@gmail.com>
2020-12-10 19:53:01 +0100

    first commit 2020-12-12

---
+++ src/main/java/io/github/agamgk/HelloServlet.java
@@ -0,0 +1,8 @@
+package io.github.agamgk;
+
+//klasa obsługująca protokół Http
+public class HelloServlet {
+    public static void main(String[] args) {
+        System.out.println("sssss");
+    }
+}
